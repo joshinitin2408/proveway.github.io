ae21d1b1e71a0bf15a45f0ef3f1983bcba0f22fc
NitinJoshi <nitin@devinfosys.co.za>
2023-08-07 18:02:23 +0530

    pixel perfect

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-const radioButtons = document.querySelectorAll('input[type="radio"][name="bundle"]');
-const selectForms = document.querySelectorAll('.select');
-const boxes = document.querySelectorAll('.box');
-const totalPriceElement = document.getElementById('total-price');
-let totalPrice = 0;
-totalPriceElement.textContent = 'DKK ' + totalPrice;
-radioButtons.forEach((radioButton, index) => {
-    radioButton.addEventListener('click', () => {
-        selectForms.forEach((selectForm) => {
-            selectForm.style.display = 'none';
-        });
-        selectForms[index].style.display = 'flex';
-        totalPrice = parseInt(radioButtons[index].getAttribute('data-price'));
-        totalPriceElement.textContent = 'DKK ' + totalPrice;
+const box1 = document.getElementById('box-1');
+const box2 = document.getElementById('box-2');
+const box3 = document.getElementById('box-3');
+const total = document.getElementById('total');
+const radioButtonsNodes = document.querySelectorAll('.radioBtn');
+const sizeColorsDivsNodes = document.querySelectorAll('.size-color-boxes-container');
+const boxesChildNode = document.querySelectorAll('.box');
 
-        // Add a class to the selected box and remove from others
-        boxes.forEach((box) => {
-            box.classList.remove('selected');
-        });
-        boxes[index].classList.add('selected');
-    });
+console.log("total: ", total);
+total.innerHTML = ""
+
+// this function will get frist box data and call boxUpdaterFunction
+box1.addEventListener('click', () => {
+    const radiobtn = radioButtonsNodes[0]; // return html node
+    const sizeBoxController = sizeColorsDivsNodes[0]; // return html node
+    // Passing 3 nodes as argument to boxUpdaterFunction
+    boxUpdaterFunction(boxesChildNode, radiobtn, sizeBoxController, box1);
+
 });
+// this function will get second box data and call boxUpdaterFunction
+box2.addEventListener('click', () => {
+    const radiobtn = radioButtonsNodes[1]; // return html node
+    const sizeBoxController = sizeColorsDivsNodes[1]; // return html node
+    // Passing 3 nodes as argument to boxUpdaterFunction
+    boxUpdaterFunction(boxesChildNode, radiobtn, sizeBoxController, box2);
+});
+// this function will get third box data and call boxUpdaterFunction
+box3.addEventListener('click', () => {
+    const radiobtn = radioButtonsNodes[2]; // return html node
+    const sizeBoxController = sizeColorsDivsNodes[2]; // return html node
+    // Passing 3 nodes as argument to boxUpdaterFunction
+    boxUpdaterFunction(boxesChildNode, radiobtn, sizeBoxController, box3);
+});
+
+
+// this function will update current box's code
+const boxUpdaterFunction = (nodes, radiobtn, sizeBoxController, box) => {
+
+    // remove active classes
+    for (let i = 0; i < nodes.length; i++) {
+        // it will remove radio button outline
+        radioButtonsNodes[i].classList.remove('active-outline');
+        // it will remove radio button's inner div's background
+        radioButtonsNodes[i].children[0].classList.remove('active-bg');
+        // It will remove active box class from all boxes
+        nodes[i].classList.remove('active-box-style')
+        // it will put display none, which is display block
+        nodes[i].children[1].style.display = 'none';
+    }
+
+    // extracting total price value from current box
+    const totalValue = box.children[0].children[0].children[1].children[1].innerHTML;
+    // adding value to total price element
+    total.innerHTML = totalValue;
+    // add active aclasses
+    radiobtn.classList.add('active-outline');
+    radiobtn.children[0].classList.add('active-bg');
+    box.classList.add('active-box-style');
+    sizeBoxController.style.display = "block";
+
+}
